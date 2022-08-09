6c85eac8741c92c644b255cf1f72e813d90e2457
Huu Chi Huynh <73843190+HuuChiHuynh@users.noreply.github.com>
2022-08-09 10:59:40 +0100

    Fixed Disposal Method & npix from frame size

--- src/Gif.rs
+++ src/Gif.rs
@@ -247,7 +247,10 @@ impl Decoder {
         Self::increment_offset(offset, 1);
 
         let packed_field = contents[*offset];
-        parsed_frame.gcd.disposal_method = (packed_field & 0b0001_1100) as u8;
+        parsed_frame.gcd.disposal_method = shr_or((packed_field & 0b0001_1100) as u32, 2, 0) as u8;
+        if (parsed_frame.gcd.disposal_method == 0) {
+            parsed_frame.gcd.disposal_method = 1; // elect to keep old image if discretionary
+        }
         parsed_frame.gcd.user_input_flag = (packed_field & 0b0000_0010) != 0;
         parsed_frame.gcd.transparent_color_flag = (packed_field & 0b0000_0001) != 0;
         Self::increment_offset(offset, 1);
@@ -314,7 +317,7 @@ impl Decoder {
             parsed_frame.local_table = local_color_vector;
         }
         let null_code: i32 = -1;
-        let npix = gif.lsd.width * gif.lsd.height;
+        let npix = parsed_frame.im.width * parsed_frame.im.height;
 
         // Initialize GIF data stream decoder.
         let lzw_minimum_code_size = contents[*offset];
